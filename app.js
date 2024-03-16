b47f84a4e0eac54c88f750e654dbd51eddee5f68
FurkanG99 <110032519+FurkanG99@users.noreply.github.com>
2024-03-16 14:27:18 +0300

    asd

--- app.js
+++ app.js
@@ -30,24 +30,6 @@ window.onclick = function(event) {
 }
 
 
-var modal2 = document.getElementById("myModal2");
-var btn2 = document.getElementById("btn2");
-var span2 = document.getElementsByClassName("close2")[0];
-
-btn2.onclick = function() {
-  modal2.style.display = "block";
-}
-
-span2.onclick = function() {
-  modal2.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
-}
-
 
 var modal3 = document.getElementById("myModal3");
 var btn3 = document.getElementById("btn3");
